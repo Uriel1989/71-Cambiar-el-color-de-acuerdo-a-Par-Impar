c66e63b9e59db3408af2a961242bc3ba1753141b
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:30:24 -0300

    Add files via upload

---
+++ 71-Cambiar el color de acuerdo a Par-Impar/ListadoDeNumeros/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListadoDeNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.CursorLeft = 30;
+
+
+            Console.WriteLine("Ingresa numeros y cambia los colores:");
+
+            Console.WriteLine("Ingresa la cantiada de numeros que quieres introducir:");
+            
+
+            int CantNum = Convert.ToInt32(Console.ReadLine());
+
+            List<int> ListNum = new List<int>();
+
+            Console.WriteLine("Ahora ingresa los numeros:");
+
+            for (int i=0; i<CantNum; i++)
+            {
+                int IngNum = Convert.ToInt32(Console.ReadLine());
+
+
+                if (IngNum % 2 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+
+                    Console.Clear(); /*El 'Clear' es lo que permite el cambio de fondo
+                                      *sin el 'Clear' el cambio de color se limita a las letras*/
+                }
+                else if (IngNum % 2 != 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.Clear();
+
+                }
+                Console.WriteLine("Sorprendido?,vamos, ingresa otro numero:");
+
+                ListNum.Add(IngNum);
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Listado de numeros ingresados:");
+
+            for (int i=0; i<ListNum.Count; i++)
+            {
+                int Listado = ListNum[i];
+
+                Console.WriteLine("El numero de la posicion"  +" "+ (i+1) + " es: " +Listado);
+
+            }
+            Console.WriteLine("La cantidad de numeros ingresados es de:" + " " + ListNum.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
